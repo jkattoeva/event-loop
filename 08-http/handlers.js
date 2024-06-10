81067a91badfde3006117ee1788596d0545939e7
Janetta Kattoeva <jkattoeva@gmail.com>
2024-06-10 17:23:29 +0600

    make a post req for comment form

--- 08-http/handlers.js
+++ 08-http/handlers.js
@@ -1,4 +1,20 @@
+const fs = require('fs')
+const qs = require('querystring')
 const comments = require('./data')
+
+function getHome(req, res) {
+  fs.readFile('./files/comment_form.html', (err, data) => {
+    if (err) {
+      res.statusCode = 500
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('server error while loading HTML file')
+    } else {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html')
+      res.end(data)
+    }
+  })
+}
 
 function getHtml(req, res) {
   res.statusCode = 200
@@ -6,7 +22,7 @@ function getHtml(req, res) {
   res.write('<html><body><div>')
   res.write('<h1>greetings from the http server!</h1>')
   res.write('</div></body></html>')
-  return res.end()
+  res.end()
 }
 
 function getText(req, res) {
@@ -21,10 +37,62 @@ function getComments(req, res) {
   res.end(JSON.stringify(comments))
 }
 
+function postComment(req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+
+  if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+
+    req.on('end', () => {
+      try {
+        const comment = qs.parse(body)
+        comments.push(comment)
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<h1>Comment data was received</h1>')
+        res.write('<a href="/">submit one more comment</a>')
+        res.end()
+      } catch (error) {
+        res.statusCode = 400
+        res.end('invalid form data')
+      }
+    })
+  } else if (req.headers['content-type'] === 'application/json') {
+    let commentJson = ''
+
+    req.on('data', (chunk) => (commentJson += chunk))
+
+    req.on('end', () => {
+      try {
+        comments.push(JSON.parse(commentJson))
+        res.statusCode = 200
+        res.end('Comment data was received')
+      } catch (error) {
+        res.statusCode = 400
+        res.end('Invalid JSON')
+      }
+    })
+  } else {
+    res.statusCode = 400
+    res.end('data must be in the JSON format')
+  }
+}
+
 function handleNotFound(req, res) {
   res.statusCode = 404
   res.setHeader('Content-Type', 'text/html')
   res.end('<h1>Page not found</h1>')
 }
 
-module.exports = { getComments, getHtml, getText, handleNotFound }
+module.exports = {
+  getComments,
+  getHtml,
+  getText,
+  handleNotFound,
+  postComment,
+  getHome,
+}
--- 08-http/index.js
+++ 08-http/index.js
@@ -3,15 +3,18 @@ const {
   getHtml,
   getText,
   handleNotFound,
+  postComment,
+  getHome,
 } = require('./handlers.js')
 
 const http = require('http')
 
 const PORT = 5000
 
-
-
 const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    return getHome(req, res)
+  }
   if (req.method === 'GET' && req.url === '/html') {
     return getHtml(req, res)
   }
@@ -23,6 +26,10 @@ const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/comments') {
     return getComments(req, res)
   } // we are adding return cause we don't want to do the code again
+
+  if (req.method === 'POST' && req.url === '/comments') {
+    return postComment(req, res)
+  }
 
   handleNotFound(req, res)
 })
